42387717652adc3f0daf1b370b6fccb063558eb6
Daniel <dgt1@sfu.ca>
2022-03-14 08:42:25 -0700

    Track player/enemy block locations

---
+++ Phase 2/RaccoonGame/src/main/java/subject/TreeMaker.java
@@ -0,0 +1,32 @@
+package subject;
+
+import main.RaccoonGame;
+
+public class TreeMaker {
+    //Some needed variables
+    Player player;
+    Enemy enemy;
+    int enemyBlockX, enemyBlockY, playerBlockX, playerBlockY;
+    RaccoonGame raccoonGame;
+
+    //Default constructor
+    TreeMaker(Player player, Enemy enemy){
+        //variable initialization from input
+        this.player = player;
+        this.enemy = enemy;
+        this.raccoonGame = player.raccoonGame;
+    }
+
+    //Update method called by enemy each update
+    public void update(){
+        blockUpdate();
+    }
+
+    //Find which blocks the enemy and player are in
+    private void blockUpdate(){
+        enemyBlockX = enemy.x / raccoonGame.blockSize;
+        enemyBlockY = enemy.y / raccoonGame.blockSize;
+        playerBlockX = player.x / raccoonGame.blockSize;
+        playerBlockY = player.y / raccoonGame.blockSize;
+    }
+}
