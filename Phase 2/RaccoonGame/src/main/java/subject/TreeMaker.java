723b2e135ba2e6d206ce39d130013bdf71921a63
Daniel <dgt1@sfu.ca>
2022-03-16 14:40:21 -0700

    Generates path from enemy to player

--- Phase 2/RaccoonGame/src/main/java/subject/TreeMaker.java
+++ Phase 2/RaccoonGame/src/main/java/subject/TreeMaker.java
@@ -2,9 +2,7 @@ package subject;
 
 import main.RaccoonGame;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 import static subject.GraphMaker.mapNodeArr;
 
@@ -40,7 +38,7 @@ public class TreeMaker {
         System.out.println("Player X: "+playerNode.x+"\nPlayer Y: "+playerNode.y);
         System.out.println("Player: "+playerNode);
         //reset the path and generate the tree
-        ArrayList<GraphMaker.Node> path = BFS();
+        List<GraphMaker.Node> path = BFS();
         //more testing stuff
         printPath(path);
         System.out.println("Generated Tree");
@@ -48,7 +46,7 @@ public class TreeMaker {
     }
 
     //Method to print the path found from the enemy to the player
-    private void printPath(ArrayList<GraphMaker.Node> path){
+    private void printPath(List<GraphMaker.Node> path){
         for(int i=0; i<path.size(); i++){
             System.out.print("("+path.get(i).x+" ,"+path.get(i).y+")");
         }
@@ -56,29 +54,25 @@ public class TreeMaker {
     }
 
     //BFS traversal for enemy pathing. Returns a root to the tree
-    private ArrayList<GraphMaker.Node> BFS() {
-        //set the current node to the root, create an empty queue, and a list for storing path
-        GraphMaker.Node current;
+    private List<GraphMaker.Node> BFS() {
+
         Queue<GraphMaker.Node> queue = new LinkedList<>();
-        ArrayList<GraphMaker.Node> path = new ArrayList<GraphMaker.Node>();
+        List<GraphMaker.Node> path = new ArrayList<>();
 
-        //mark all nodes unvisited
-        for (int row = 0; row < raccoonGame.windowRow; row++) {
-            for (int col = 0; col < raccoonGame.windowCol; col++) {
-                mapNodeArr[col][row].visited = false;
-            }
-        }
-
-        //Insert the root into the queue
         queue.add(root);
-        while(!queue.isEmpty()) {
-            //Let current be the vertex and the front of the queue and remove current from the queue, also mark current as visited
+        root.visited = true;
+
+        Map<GraphMaker.Node, GraphMaker.Node> parentMap = new HashMap<>();
+
+        GraphMaker.Node current = root;
+        parentMap.put(root, null);
+        while(!queue.isEmpty()){
             current = queue.poll();
-            path.add(current);
             current.visited = true;
 
-            if (current == playerNode) {
-                return path;
+            if(current == playerNode){
+                System.out.println("HERE");
+                break;
             }
 
             //Make a list of adjacent nodes to current
@@ -90,13 +84,18 @@ public class TreeMaker {
 
             //Iterate through, check if visited, if not add to queue
             for (GraphMaker.Node neighbor : adjacent) {
-                if (neighbor != null && !neighbor.visited) {
+                if (!neighbor.visited && neighbor.isZero) {
                     queue.add(neighbor);
+                    parentMap.put(neighbor, current); // mark current node as parent to neighbor
                 }
             }
         }
-        //No path found at this point, so return null
-        return null;
+        GraphMaker.Node currIter = current;
+        while (currIter != null) {
+            path.add(0, currIter);
+            currIter = parentMap.get(currIter);
+        }
+        return path;
     }
 
 
