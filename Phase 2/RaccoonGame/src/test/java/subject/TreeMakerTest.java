42ffa9e0adce8f1e391b4a64d8ae816a5a927de0
Daniel <dgt1@sfu.ca>
2022-04-03 01:26:52 -0700

    all tests pass

--- Phase 2/RaccoonGame/src/test/java/subject/TreeMakerTest.java
+++ Phase 2/RaccoonGame/src/test/java/subject/TreeMakerTest.java
@@ -11,6 +11,9 @@ public class TreeMakerTest {
     @Test
     public void MakeTree(){
         RaccoonGame raccoonGame = new RaccoonGame();
+        for (int i = 0; i < raccoonGame.enemyHandler.EnemyList.size()-1; i++) {
+            raccoonGame.enemyHandler.EnemyList.get(i).speed = 0;
+        }
         raccoonGame.gameState = raccoonGame.playState;
         Assert.assertEquals(5 ,raccoonGame.enemyHandler.EnemyList.size());
 
@@ -33,16 +36,17 @@ public class TreeMakerTest {
     @Test
     public void updateTest(){
         RaccoonGame raccoonGame = new RaccoonGame();
-        raccoonGame.gameState = raccoonGame.playState;
+        for (int i = 0; i < raccoonGame.enemyHandler.EnemyList.size()-1; i++) {
+            raccoonGame.enemyHandler.EnemyList.get(i).speed = 0;
+        }
         raccoonGame.player.x = 476;
         raccoonGame.player.y = 480;
         try {
             for (int i = 0; i < raccoonGame.enemyHandler.EnemyList.size()-1; i++){
-                raccoonGame.enemyHandler.EnemyList.get(i).speed = 0;
                 List<GraphMaker.Node> list = raccoonGame.enemyHandler.EnemyList.get(i).tree.update();
                 Assert.assertNotEquals("", raccoonGame.enemyHandler.EnemyList.get(i).tree.print(list));
                 if(i==1){
-                    Assert.assertEquals("(18 ,11)(18 ,12)(18 ,13)(17 ,13)(16 ,13)(15 ,13)(15 ,14)(15 ,15)(14 ,15)", raccoonGame.enemyHandler.EnemyList.get(i).tree.print(list));
+                    Assert.assertEquals("(17 ,11)(17 ,12)(17 ,13)(16 ,13)(15 ,13)(15 ,14)(15 ,15)(14 ,15)", raccoonGame.enemyHandler.EnemyList.get(i).tree.print(list));
                 }
             }
             return;
