9ff7199c18e78e2f957db61752570b7abc8f4470
Ryan Wittkopf <rwa116@sfu.ca>
2022-04-06 18:02:20 -0700

    Refactoring: introducing sub-classes into keyHandler based on the current gameState similarly to how the gui update function works, getting rid of an overly large class.

--- Phase 2-3/RaccoonGame/src/main/java/main/KeyHandler.java
+++ Phase 2-3/RaccoonGame/src/main/java/main/KeyHandler.java
@@ -4,6 +4,7 @@ import javax.sound.midi.SysexMessage;
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.security.Key;
 
 /**
  * Delegates certain keys into in-game commands.
@@ -25,101 +26,130 @@ public class KeyHandler implements KeyListener {
     public void keyTyped(KeyEvent e) {
     }
 
+
     /**
-     * Takes in a KeyEvent parameter which is the key that is has been pressed by the user.
-     * Depending on the current gameState of the game the key press will set a variety of attributes.
-     * In pauseState, titleState, and endState these key presses affect the games GUI.
-     * In playState these keyPresses affect the KeyHandler's upPressed, downPressed, leftPressed,
-     * and rightPressed boolean attributes to determine which direction the player wants to move in.
+     * Calls various sub-methods depending on the current gameState of RaccoonGame. Also calculates
+     * the integer keyCode from the given KeyEvent and passes this to the sub-methods for processing.
+     * @param e: takes in a KeyEvent object e.
      */
     @Override
     public void keyPressed(KeyEvent e) {
-        int keyCode = e.getKeyCode();       //keyCode is the ASCII value of a key on the keyboard
+        int keyCode = e.getKeyCode(); //keyCode is the ASCII value of a key on the keyboard
 
-        //title state
         if(raccoonGame.gameState == raccoonGame.titleState) {
-            if(keyCode == KeyEvent.VK_W){      //If w pressed
-                if(raccoonGame.gui.cursorNum > 0) {
-                    raccoonGame.gui.cursorNum--;
-                }
-            }
-            if(keyCode == KeyEvent.VK_S){      //if s pressed
-                if(raccoonGame.gui.cursorNum < 1) {
-                    raccoonGame.gui.cursorNum++;
-                }
-            }
-
-            if(keyCode == KeyEvent.VK_ENTER) {
-                switch (raccoonGame.gui.cursorNum) {
-                    case 0:
-                        raccoonGame.gameState = raccoonGame.playState;
-                        raccoonGame.sound.stop(raccoonGame.sound);
-                        raccoonGame.sound.music(0, raccoonGame.sound);
-                        break;
-                    case 1:
-                        System.exit(0);
-                        break;
-                }
-            }
+            keyPressedTitle(keyCode);
         }
 
-        //end state
         else if(raccoonGame.gameState == raccoonGame.endState) {
-
-            if(keyCode == KeyEvent.VK_ENTER) {
-                System.exit(0);
-                
-            }
+            keyPressedEnd(keyCode);
         }
 
-        //pause state
         else if(raccoonGame.gameState == raccoonGame.pauseState) {
-            if(keyCode == KeyEvent.VK_ESCAPE){      //if esc pressed
-                raccoonGame.gameState = raccoonGame.playState;
-            }
-            if(keyCode == KeyEvent.VK_W){      //If w pressed
-                if(raccoonGame.gui.cursorNum > 0) {
-                    raccoonGame.gui.cursorNum--;
-                }
-            }
-            if(keyCode == KeyEvent.VK_S){      //if s pressed
-                if(raccoonGame.gui.cursorNum < 1) {
-                    raccoonGame.gui.cursorNum++;
-                }
-            }
-
-            if(keyCode == KeyEvent.VK_ENTER) {
-                switch (raccoonGame.gui.cursorNum) {
-                    case 0:
-                        raccoonGame.gameState = raccoonGame.playState;
-                        break;
-                    case 1:
-                        System.exit(0);
-                        break;
-                }
-            }
+            keyPressedPause(keyCode);
         }
 
-        //play state
         else if(raccoonGame.gameState == raccoonGame.playState) {
-            //now scan to check which key has been pressed
-            if(keyCode == KeyEvent.VK_W){      //If w pressed
-                upPressed = true;
+            keyPressedPlay(keyCode);
+        }
+    }
+
+    /**
+     * Controls the game for the title screen. Checks for input of W or S keys to move the cursor
+     * or ENTER key to either start or quit the game depending on the current cursor placement.
+     * @param keyCode: integer keyCode corresponding to the key pressed.
+     */
+    public void keyPressedTitle(int keyCode ) {
+        if(keyCode == KeyEvent.VK_W){      //If w pressed
+            if(raccoonGame.gui.cursorNum > 0) {
+                raccoonGame.gui.cursorNum--;
             }
-            if(keyCode == KeyEvent.VK_S){      //if s pressed
-                downPressed = true;
-            }
-            if(keyCode == KeyEvent.VK_A){      //if a pressed
-                leftPressed = true;
-            }
-            if(keyCode == KeyEvent.VK_D){      //if d pressed
-                rightPressed = true;
-            }
-            if(keyCode == KeyEvent.VK_ESCAPE){      //if esc pressed
-                raccoonGame.gameState = raccoonGame.pauseState;
+        }
+        if(keyCode == KeyEvent.VK_S){      //if s pressed
+            if(raccoonGame.gui.cursorNum < 1) {
+                raccoonGame.gui.cursorNum++;
             }
         }
 
+        if(keyCode == KeyEvent.VK_ENTER) {
+            switch (raccoonGame.gui.cursorNum) {
+                case 0:
+                    raccoonGame.gameState = raccoonGame.playState;
+                    raccoonGame.sound.stop(raccoonGame.sound);
+                    raccoonGame.sound.music(0, raccoonGame.sound);
+                    break;
+                case 1:
+                    System.exit(0);
+                    break;
+            }
+        }
+    }
+
+    /**
+     * Controls the game for the pause screen. Checks for input of W or S keys to move the cursor
+     * or ENTER key to either resume or quit the game depending on the current cursor placement.
+     * @param keyCode: integer keyCode corresponding to the key pressed.
+     */
+    public void keyPressedPause(int keyCode) {
+        if(keyCode == KeyEvent.VK_ESCAPE){      //if esc pressed
+            raccoonGame.gameState = raccoonGame.playState;
+        }
+        if(keyCode == KeyEvent.VK_W){      //If w pressed
+            if(raccoonGame.gui.cursorNum > 0) {
+                raccoonGame.gui.cursorNum--;
+            }
+        }
+        if(keyCode == KeyEvent.VK_S){      //if s pressed
+            if(raccoonGame.gui.cursorNum < 1) {
+                raccoonGame.gui.cursorNum++;
+            }
+        }
+
+        if(keyCode == KeyEvent.VK_ENTER) {
+            switch (raccoonGame.gui.cursorNum) {
+                case 0:
+                    raccoonGame.gameState = raccoonGame.playState;
+                    break;
+                case 1:
+                    System.exit(0);
+                    break;
+            }
+        }
+    }
+
+    /**
+     * Controls the game for the play screen. Checks for input of W, A, S, and D keys and set's the
+     * keyHandler's upPressed, downPressed, leftPressed, and rightPressed boolean attributes to true
+     * respectively. Also check's for ESCAPE in order to pause the game.
+     * @param keyCode: integer keyCode corresponding to the key pressed.
+     */
+    public void keyPressedPlay(int keyCode) {
+        //now scan to check which key has been pressed
+        if(keyCode == KeyEvent.VK_W){      //If w pressed
+            upPressed = true;
+        }
+        if(keyCode == KeyEvent.VK_S){      //if s pressed
+            downPressed = true;
+        }
+        if(keyCode == KeyEvent.VK_A){      //if a pressed
+            leftPressed = true;
+        }
+        if(keyCode == KeyEvent.VK_D){      //if d pressed
+            rightPressed = true;
+        }
+        if(keyCode == KeyEvent.VK_ESCAPE){      //if esc pressed
+            raccoonGame.gameState = raccoonGame.pauseState;
+        }
+    }
+
+    /**
+     * Controls the game for the end screen. Monitors for ENTER which exits the program.
+     * @param keyCode: integer keyCode corresponding to the key pressed.
+     */
+    public void keyPressedEnd(int keyCode) {
+        if(keyCode == KeyEvent.VK_ENTER) {
+            System.exit(0);
+
+        }
     }
 
     /**
